1f7d0db6624a67b21fea12697b5a72efccd6ccee
Carlos Montenegro <cgmonte@gmail.com>
2021-10-01 13:14:54 -0300

    navigation function component

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,9 @@
-import React from "react";
-
-class Navigation extends React.Component {
-    render() {
-        return (
-            <div>
-                <a href={"/"}>Voltar</a>
-            </div>
-        );
-    }
+function Navigation() {
+    return (
+    <div>
+        <a href={"/"}>Voltar</a>
+    </div>
+    );
 }
 
 export default Navigation
