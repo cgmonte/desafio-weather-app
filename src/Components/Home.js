f350eef67664012059b57278b203c1d08d0c69aa
Carlos Montenegro <cgmonte@gmail.com>
2021-10-04 21:02:33 -0300

    uzando fragments na Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,37 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { Link, withRouter } from 'react-router-dom';
-class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.links = this.props.cities.map((city) =>
+
+const Home = ({cities}) => (
+    <Fragment>
+        <h1>
+            WEATHER
+        </h1>
+
+        <h3>select a city</h3>
+
+        {cities.map((city) =>
             <div key={city}>
-                <Link
-                    to={`/${city}`}
-                    className="home-links"
-                >
+                <Link to={`/${city}`} className="home-links">
                     {city}
                 </Link>
             </div>
-        );
-    }
-
-    render() {
-
-        return (
-            <div>
-                <h1>
-                    WEATHER
-                </h1>
-
-                <h3>
-                    select a city
-                </h3>
-
-                {this.links}
-            </div>
-        );
-    }
-}
-
-export default withRouter(Home);
+        )}
+    </Fragment>
+)
+export default withRouter(Home); 
