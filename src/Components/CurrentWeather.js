2b9a0c274bd293f9c6f125213e5ec3b4a518646e
Carlos Montenegro <cgmonte@gmail.com>
2021-10-04 21:14:44 -0300

    current: destruct das props na definição da função

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -4,53 +4,48 @@ import Icon from "./Icon";
 
 import { HiOutlineArrowNarrowUp, HiOutlineArrowNarrowDown } from "react-icons/hi";
 
-function CurrentWeather(props) {
+const CurrentWeather = ({isDay, data: {current_condition, current_temperature, today_min_temperature, today_max_temperature, current_condition_code}}) => (
+    <div id="current-weather-div">
 
-    return (
-        <div id="current-weather-div">
+    <p id="current-condition-title">{current_condition.toLowerCase()}</p>
 
-            <p id="current-condition-title">{props.data.current_condition.toLowerCase()}</p>
+    <div id="current-weather-info-div">
 
+        <p id="current-temp-data">{Math.trunc(current_temperature)}</p>
 
-            <div id="current-weather-info-div">
-
-                <p id="current-temp-data">{Math.trunc(props.data.current_temperature)}</p>
-
-                <div id="current-temp-details">
+        <div id="current-temp-details">
 
 
-                    <div id="current-temp-unit">
-                        <p>&deg;C</p>
-                    </div>
+            <div id="current-temp-unit">
+                <p>&deg;C</p>
+            </div>
 
-                    <div className="grow-box"></div>
+            <div className="grow-box"></div>
 
-                    <div className="current-max-data">
+            <div className="current-max-data">
 
-                        <HiOutlineArrowNarrowUp />
-                        <p>{Math.trunc(props.data.today_max_temperature)}&deg;</p>
-
-                    </div>
-
-
-                    <div className="current-min-data">
-
-                        <HiOutlineArrowNarrowDown />
-                        <p>{Math.trunc(props.data.today_min_temperature)}&deg;</p>
-
-                    </div>
-
-                </div>
-
+                <HiOutlineArrowNarrowUp />
+                <p>{Math.trunc(today_max_temperature)}&deg;</p>
 
             </div>
 
-            <div id="current-weather-icon-div">
-                <Icon isDay={props.isDay} conditionCode={props.data.current_condition_code} />
+
+            <div className="current-min-data">
+
+                <HiOutlineArrowNarrowDown />
+                <p>{Math.trunc(today_min_temperature)}&deg;</p>
+
             </div>
 
         </div>
-    );
-}
+
+    </div>
+
+    <div id="current-weather-icon-div">
+        <Icon isDay={isDay} conditionCode={current_condition_code} />
+    </div>
+
+</div>
+)
 
 export default CurrentWeather
